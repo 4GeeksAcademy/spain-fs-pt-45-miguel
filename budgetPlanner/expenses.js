c1f4bb70958d944e8760f10b2d4afb01ddecc2ed
Miguel Angel Martinez Pazos <92331020+MPMiguel@users.noreply.github.com>
2023-07-13 10:37:01 +0000

    fix

--- budgetPlanner/expenses.js
+++ budgetPlanner/expenses.js
@@ -1,13 +1,14 @@
-function regularExpenses(expenses, description, amount, date) {
+const listOfExpenses = [];
+
+function regularExpenses(category, description, amount, date) {
   const newExpense = {
+    category: category,
     description: description,
     amount: amount,
     date: date
   };
-  expenses.push(newExpense);
+  listOfExpenses.push(newExpense);
   return newExpense;
 }
 
-console.log(regularExpenses('cafe','4kilos',20,'13/7/23'));
-
-  
+console.log(regularExpenses('cafe', '4kilos', 20, '13/7/23'));
