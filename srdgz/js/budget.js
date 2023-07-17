563d60c8c349aee5906776c3b0bf353d5a2add84
Sandra Rodríguez <133523307+srdgz@users.noreply.github.com>
2023-07-17 08:47:42 +0000

    structure modification

--- srdgz/js/budget.js
+++ srdgz/js/budget.js
@@ -1,13 +1,23 @@
-const food = [];
-const services = [];
-const housing = [];
-const education = [];
+let categories = ['food', 'services', 'housing', 'education'];
+let categoriesData = [[], [], [], []];
+let textCategories = '';
 
+// Print list of categories
+function printCategories(){
+    textCategories = 'Enter the category: '
+    for (let i = 0; i < categories.length; i++){
+        textCategories += (i + 1) + '-' + categories[i] + ' ';
+    }
+    return textCategories;
+}
+
+// Add expense to a category
 function addExpenses (array, amount) {
     array.push(-Math.abs(amount));
     return array;
 }
 
+// Delete expense from a category
 function deleteExpenses (array, amount) {
     array.splice(array.indexOf(-Math.abs(amount)), 1);
     return array;
@@ -17,40 +27,13 @@ const prompt = require('prompt-sync')({ sigint: true });
 let retry = 1;
 while (retry == 1) {        
     let option = prompt('What do you want to do?: 1-Add, 2-Delete: ');
-    let category = prompt('Enter the category: 1-Food, 2-Services, 3-Housing, 4-Education: ');
+    let category = prompt(printCategories());
+    category = category - 1;
     let amount = prompt('Enter the amount: ');
     if (option == 1) {
-        switch (category){
-            case "1":
-                console.log("Food: " + addExpenses(food,amount));
-                break;
-            case "2":
-                console.log("Services: " + addExpenses(services,amount));
-                break;
-            case "3":
-                console.log("Housing: " + addExpenses(housing,amount));
-                break;
-            case "4":
-                console.log("Education: " + addExpenses(education,amount));
-            default:
-                console.log("Introduce a correct number: ")
-        }
+        console.log(categories[category] + ': ' + addExpenses(categoriesData[category], amount));
     } else {
-        switch (category){
-            case "1":
-                console.log("Food: " + deleteExpenses(food,amount));
-                break;
-            case "2":
-                console.log("Services: " + deleteExpenses(services,amount));
-                break;
-            case "3":
-                console.log("Housing: " + deleteExpenses(housing,amount));
-                break;
-            case "4":
-                console.log("Education: " + deleteExpenses(education,amount));
-            default:
-                console.log("Introduce a correct number: ")
-        }
+        console.log(categories[category] + ': ' + deleteExpenses(categoriesData[category], amount));
     }
-    retry = prompt('Do you want to perform another operation?: 1-Yes, 2-No: ');
+    retry = prompt('Do you want to perform another operation?: 1-Yes, 2-No ');
 }
