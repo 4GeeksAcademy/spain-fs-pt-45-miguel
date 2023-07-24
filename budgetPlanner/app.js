432d9e049517e846e5eaad6b25adab38555c5782
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-07-24 18:41:38 +0000

    Footer and boody

--- budgetPlanner/app.js
+++ budgetPlanner/app.js
@@ -1,5 +1,10 @@
 import { alert } from "./components/alert.js";
+import { body } from "./components/body.js";
+import { footer } from "./components/footer.js";
+
 
 const root = document.querySelector('#root');
 
 root.append(alert('primary', 'Todo ha ido bien!'));
+root.append(body());
+root.append(footer(`<h1>Hola clase!</h1>`));
