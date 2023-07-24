9a5ee86fe8cf90c1f6914f3fbaa410344945dc48
Miguel Angel Martinez Pazos <92331020+MPMiguel@users.noreply.github.com>
2023-07-24 18:09:24 +0000

    navbar

--- budgetPlanner/app.js
+++ budgetPlanner/app.js
@@ -1,5 +1,7 @@
 import { alert } from "./components/alert.js";
+import { navbar } from "./components/navbar.js";
 
 const root = document.querySelector('#root');
 
 root.append(alert('primary', 'Todo ha ido bien!'));
+root.append(navbar());
